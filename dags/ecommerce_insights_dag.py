46d6f3c056bc8a43ef428b539cc4cf5fcb34d4ec
imad abodehn <imadomar64@gmail.com>
2024-08-09 04:32:55 +0200

    dag

--- dags/ecommerce_insights_dag.py
+++ dags/ecommerce_insights_dag.py
@@ -1,59 +1,77 @@
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
-from google.cloud import storage
-import pandas as pd
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+from pyspark.sql import SparkSession, DataFrame, functions as sf
+
+GCS_BUCKET = "ecommerce-customer-bucket"
+
+def read_data(spark: SparkSession, file_path: str) -> DataFrame:
+    """reads csv file from specified file path."""
+    return spark.read.csv(file_path, header=True, inferSchema=True)
+
+def calculate_highest_spend(df: DataFrame) -> DataFrame:
+    membership_by_gender = (df.groupBy("Gender", "Membership Type")
+                          .agg(sf.round(sf.mean("age"), 1).alias("Average_age"),
+                                sf.round(sf.mean("Items Purchased"), 0).alias("Average_items_purchased"),
+                               sf.round(sf.mean("Total Spend"), 2).alias("Average_spend"))
+                        .sort("Average_spend", ascending=False))
+    return membership_by_gender
+
+def write_to_gcs(df: DataFrame, filepath: str):
+    return (df
+            .toPandas()
+            .to_csv(filepath, index = False))
+
+def etl_with_spark():
+    spark = SparkSession.builder \
+    .appName('data-engineering-capstone') \
+    .config("spark.jars", "https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop3-latest.jar") \
+    .config("spark.sql.repl.eagerEval.enabled", True) \
+    .getOrCreate()
+
+    file_path = f"gs://ecommerce-customer-bucket/e-commerce-customer-behavior.csv"
+    # read data from gcs
+    customer_df = read_data(spark=spark, file_path=file_path)
+
+    # perform some transformations
+    insights_df = calculate_highest_spend(df=customer_df)
+
+    # Write insights to GCS
+    datetime_now = datetime.now().strftime("%m%d%Y%H%M%S")
+
+    write_path = f"gs://project_imad/insights/{datetime_now}.csv"
+
+    write_to_gcs(df=insights_df, filepath=write_path)
+
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2024, 1, 1),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "depends_on_past": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
-dag = DAG(
-    'ecommerce_insights',
-    default_args=default_args,
-    description='Generate e-commerce insights daily at 10 am',
-    schedule_interval='0 10 * * *',
+with DAG(
+    dag_id="ecommerce_insights",
+    start_date=datetime(2024, 7, 14),
+    schedule_interval="0 10 * * *",  # Daily interval at 10am
     catchup=False,
-)
+    tags=[
+        "Orchestration",
+        "Customer Insights",
+        "Ecommerce"
+    ],
 
-def read_data_from_gcs(bucket_name, source_blob_name):
-    client = storage.Client()
-    bucket = client.get_bucket(bucket_name)
-    blob = bucket.blob(source_blob_name)
-    data = blob.download_as_string()
-    df = pd.read_csv(pd.compat.StringIO(data.decode('utf-8')))
-    return df
+):
+    etl_with_spark = PythonOperator(
+        task_id="etl_with_spark", python_callable=etl_with_spark
+    )
 
-def process_insights():
-    df = read_data_from_gcs('gs://ecommerce-customer-bucket', 'e-commerce-customer-behavior.csv')
-    
-    # Perform customer segmentation
-    df['SpendingCategory'] = df['Total Spend'].apply(lambda x: 'High' if x > 1000 else 'Medium' if x > 500 else 'Low')
-    
-    # Identify inactive and recent customers
-    df['Days Since Last Purchase'] = df['Days Since Last Purchase'].astype(int)
-    inactive_customers = df[df['Days Since Last Purchase'] > 30]
-    recent_customers = df[df['Days Since Last Purchase'] <= 30]
-    
-    write_data_to_gcs(inactive_customers, 'gs://project_imad', 'insights/inactive_customers.csv')
-    write_data_to_gcs(recent_customers, 'gs://project_imad', 'insights/recent_customers.csv')
+    does_nothing = EmptyOperator(task_id="does_nothing")
 
-def write_data_to_gcs(df, bucket_name, destination_blob_name):
-    client = storage.Client()
-    bucket = client.get_bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
-    blob.upload_from_string(df.to_csv(index=False), 'text/csv')
+    etl_with_spark >> does_nothing
 
-process_task = PythonOperator(
-    task_id='process_insights',
-    python_callable=process_insights,
-    dag=dag,
-)
 
-process_task
+
+
